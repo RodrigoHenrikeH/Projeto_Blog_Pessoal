547a7c73da10e7d635e3aca80a3c4477f9267e37
Rodrigo Henrique <rhs.buiu@gmail.com>
2025-04-14 16:25:16 -0300

    Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -13,49 +13,56 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 // usando "@" estamos fazendo Anotações, que no caso saõ criar regras para o banco de dados
-@Entity //Usamos o @entity determinar que essa classe (postagens) vai ser uma tabela
-@Table(name = "tb_postagens") //aqui escolhemos o nome da nossa tabela, atribuindo um nome dentro do parametro ( regra entre parenteses)
+@Entity // Usamos o @entity determinar que essa classe (postagens) vai ser uma tabela
+@Table(name = "tb_postagens") // aqui escolhemos o nome da nossa tabela, atribuindo um nome dentro do
+								// parametro ( regra entre parenteses)
 public class Postagem {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank
 	@Size(min = 3, max = 100)
 	private String titulo;
-	
+
 	@NotBlank
 	@Size(min = 10, max = 1000)
 	private String texto;
-	
+
 	@UpdateTimestamp
 	private LocalDate data;
-	
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public LocalDate getData() {
 		return data;
 	}
+
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
-	
+
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-	
-	
+
 }
